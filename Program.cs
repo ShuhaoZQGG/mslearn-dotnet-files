4a8ab1028f6b3b01996cd84f6f5e50430d18af52
Shuhao Zhang <shuhao.zhang@amplexor.com>
2022-01-07 14:31:15 -0500

    write the total number of charcaters in json files into a text file

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
-﻿using System.IO;
+﻿// See https://aka.ms/new-console-template for more information
+using System.IO;
 using System.Collections.Generic;
-// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
 var currentDirectory = Directory.GetCurrentDirectory();
 var storesDirectory = Path.Combine(currentDirectory, "stores");
 var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
 Directory.CreateDirectory(salesTotalDir);
 var salesFiles = FindFiles(storesDirectory);
-File.WriteAllText(Path.Combine(salesTotalDir, "total.txt"), "does it change?");
+var salesTotal = CalculateSalesTotal(salesFiles);
+File.AppendAllText(Path.Combine(salesTotalDir, "total.txt"), $"{salesTotal}{Environment.NewLine}");
 
 foreach (var file in salesFiles)
 {
@@ -19,6 +21,30 @@ IEnumerable<string> FindFiles(string folderName)
 
     var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
     
-
+     foreach (var file in foundFiles)
+     {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
+        {
+            salesFiles.Add(file);
+        }
+      }
     return salesFiles;
 }
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+
+    foreach(var salesFile in salesFiles)
+    {
+        string salesJson = File.ReadAllText(salesFile);
+
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+record SalesData(double Total);
